9458a97919b054b1c7c2bd6493619f22aec63434
Stefan <s.vladulescu@gmail.com>
2022-03-29 19:28:01 +0300

    Test

--- script.js
+++ script.js
@@ -1,29 +1,58 @@
-var firstNumber;
-var secondNumber;
-var displayValue="";
-var result = 0;
-const operator = "";
-function display(displayValue){
-    document.querySelector(".display").innerText = displayValue;
-}
-
+var firstNumber = 0;
+var secondNumber = 0;
+var eq = 0;
 
 
 let keys = document.querySelectorAll(".btns");
 for(i of keys){
     let val = i.getAttribute("data-value");
     i.addEventListener("click", function(){
-        append(val);
+        
         firstNumber = parseInt(val);
-         
-        firstNumber = val;     
+        document.querySelector(".result").innerText = val;
+
+       
     });
     
 };
 
-function append(val){
-    displayValue = displayValue + val;
-    display(displayValue);
-    console.log(displayValue);
+
+function add(){
+    //console.log("The add function is successfully invoked");
+    
+    eq = eq+firstNumber;
+   
+
+
 }
- 
+
+function multiply(){
+    //console.log("The multiply function is successfully invoked");
+
+}
+
+function substract(){
+    //console.log("The substract function is successfully invoked");
+
+}
+
+function divide(){
+    //console.log("The divide function is successfully invoked");
+
+}
+
+function clearN(){
+    console.log("The clear function is successfully invoked");
+    firstNumber =0;
+    secondNumber =0;
+    eq = 0;
+}
+
+function equals(){
+    //console.log("The equals function is successfully invoked");
+    add(firstNumber);
+    document.querySelector(".result").innerText = eq;
+    console.log(eq)
+    clearN();
+
+}
